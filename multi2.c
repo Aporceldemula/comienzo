6a1bf071497deb0ee077fbf338c644cf4695c51b
antonio <antonioporcel@outlook.es>
2018-03-13 20:28:21 +0100

    variables

---
+++ multi2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*multiplicar 121x132 metiendo los valores en variables*/
+
+int main(){
+
+    int factor1=121;
+    int factor2=132;
+    int producto=factor1*factor2;
+
+    printf("%d x %d = %d \n", factor1, factor2, producto);
+
+
+}
