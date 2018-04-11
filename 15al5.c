1020ee885e9c9207227ae84246228c52b29f71c5
aporceldemula <antonioporcel@outlook.es>
2018-04-11 12:40:55 +0200

    for

---
+++ 15al5.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+/*programa que saque por pantalla del 15 al 5*/
+int main(){
+
+  /*inicializacion*/
+  int i;
+
+  /*condicion*/
+   for(i=15;i>=5;i--){ 
+    printf("%d ", i);
+   }
+
+    printf("\n");
+
+
+}
---
+++ 8pares.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+/*programa que saque los primeros 8 pares*/
+
+int main(){
+
+  /*inicializacion*/
+  int i; 
+
+  /*condicion*/
+   for(i=2;i<=16;i=i+2){
+       printf("%d ", i);
+   }
+   
+    printf("\n");
+
+
+}
